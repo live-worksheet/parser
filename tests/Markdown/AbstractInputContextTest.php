5010cddb5c4a50c1b4634cec46b68af1d0771190
M. Vondano <m-vo@users.noreply.github.com>
2020-11-09 12:08:42 +0100

    add input context concept

---
+++ tests/Markdown/AbstractInputContextTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * (c) Moritz Vondano
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace LiveWorksheet\Parser\Tests\Markdown;
+
+use LiveWorksheet\Parser\Markdown\AbstractInputContext;
+use LiveWorksheet\Parser\Parameter\Parameter;
+use PHPUnit\Framework\TestCase;
+
+class AbstractInputContextTest extends TestCase
+{
+    public function testSetAndGetData(): void
+    {
+        $input = $this->getMockForAbstractClass(AbstractInputContext::class);
+
+        $input->setContent("test\ncontent\n");
+
+        $input->setResources([
+            'res/foo.svg' => 'path/to/res/foo.svg',
+        ]);
+
+        $input->setParameters([
+            new Parameter('X', '1 + 1'),
+        ]);
+
+        self::assertEquals("test\ncontent\n", $input->getContent());
+
+        self::assertEquals('X', $input->getVariable('X'));
+        self::assertEquals('', $input->getVariable('Y'));
+
+        self::assertEquals('X', $input->getVariablePlaceholder('X'));
+        self::assertEquals('', $input->getVariablePlaceholder('Y'));
+
+        self::assertEquals('path/to/res/foo.svg', $input->getResourcePath('res/foo.svg'));
+        self::assertEquals('', $input->getResourcePath('res/invalid.svg'));
+    }
+}
